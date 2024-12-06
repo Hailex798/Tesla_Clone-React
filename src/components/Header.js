7324ca75cac15f2b973d1754d813ce7d41591c54
Hailex <kshitij.singh.bisht.174sis@gmail.com>
2024-12-06 21:11:16 +0530

    Added the Incomplete Navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+// import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   return (
-    <div>Header</div>
+        <Container>
+            <a>
+                    <img src="/images/logo.svg" alt="Tesla Logo" />
+            </a>
+            <Menu>
+                <a href="">Model S</a>
+                <a href="">Model Y</a>
+                <a href="">Model 3</a>
+                <a href="">Model X</a>
+            </Menu>
+            <RightMenu>
+                    <a href="">Shop</a>
+                    <a href="">Tesla Account</a>
+                    {/* <MenuIcon /> */}
+            </RightMenu>
+        </Container>
   )
 }
+
+const Container = styled.div`
+        min-height: 60px;
+        position: fixed;
+        display: flex;
+        align-items: center;
+        padding: 0 20px;
+        top: 0;
+        left: 0;
+        right: 0;
+
+        a{
+                font-weight: 600;
+                text-transform: uppercase;
+                padding: 0 10px;
+                flex-wrap: no-wrap;
+        }
+`;
+
+const Menu = styled.div`
+        display: flex;
+        align-item: center;
+        justify-content: center;
+        flex: 1;
+`;
+
+const RightMenu = styled.div`
+        a{
+                margin-right: 10px;
+        }
+`;
 
 export default Header
